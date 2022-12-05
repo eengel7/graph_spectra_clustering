99e5c9b657ee7d55c2412a6ae14e44fdd85f9cee
eengel7 <75041535+eengel7@users.noreply.github.com>
2022-12-05 19:05:36 +0100

    Final changes + added figures

--- src/dataset_reader.py
+++ src/dataset_reader.py
@@ -9,18 +9,18 @@ def read_graph(file_name: str, weighted: bool = False)-> nx.Graph:
     Args:
         file_name: File name (Here, 'example1.dat')
     Returns:
-        List of edges that are described as a list of two nodes
+        List of edges that are described as a list of two nodes and optional a corresponding edge weight 
     """
 
-    # Define the relative path to the zipped data
+    # Define the relative path to the data
     file_dir = path.dirname(__file__)   
     rel_path = "../data/" + file_name
     dataset_path = path.join(file_dir, rel_path)
 
-    # Read the data set and save them in a list 
+    # Read the data set and create a networkx graph
 
     if weighted:
-        graph = nx.read_edgelist(path = dataset_path, nodetype=int, delimiter=',')
+        graph = nx.read_edgelist(path = dataset_path, nodetype=int, delimiter=',',data=(("weight", float),))
     else:
         graph = nx.read_edgelist(path = dataset_path, nodetype=int, delimiter=',')
     
--- src/spectral_clustering.py
+++ src/spectral_clustering.py
@@ -10,51 +10,46 @@ from dataset_reader import read_graph
 
 class Spectral_clustering:
     '''
-    TODO
+    This class clusters a given graph by considering spectral information. It follows the algorithm proposed in "On Spectral Clustering: Analysis and an algorithm" by Andrew Y. Ng, Michael I. Jordan, and Yair Weiss.
     '''
 
     def __init__(self, G: nx.Graph) -> None:
             '''
-            Initialization of the class function with defined parameters 
-            :param M: number of edges in our reservoir sample stored in memory
-            :param edge_stream: edge stream being processed 
+            Initialization of the class function that uses the networkx graph for initialisation
+            :param G: NetworkX graph (weighted and unweighted)
+
             '''
             self.G = G
             self.A = nx.to_numpy_matrix(self.G)
             self.D = np.diagflat(np.sum(self.A, axis=1))
 
 
-
-    # selection_methods: Dict[str, Callable[[np.ndarray, int], int]] = {
-    #     # +2 because 1 accounts for indices starting from 0 and 1 accounts for the fact that k is the index of the NEXT
-    #     # eigenvalue
-    #     'auto': lambda eigenvalues, k: np.argmin(np.ediff1d(eigenvalues)) + 1,
-    #     'manual': lambda eigenvalues, k: k
-    # }   
-
     def compute_Laplacian(self) -> np.matrix:
+        """
+        This function computes the normalised Laplacian matrix L=D^(-1/2)AD^(-1/2).
+    
+        :return: returns the normalised Laplacian as a numpy matrix of shape (number of vertices, number of vertices)
+        """
         D_inv = np.linalg.inv(np.sqrt(self.D))
-        # L = D_inv @ self.A @ D_inv           #infix operator that is designated to be used for matrix multiplication
-
         L = np.dot(D_inv, self.A).dot(D_inv)
 
         return L
 
     def spectral_clustering(self, get_optimal_k: bool = True, k: int = 10) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
         """
-        TODO (change text)
-        This function implements the algorithm described in
-        “On Spectral Clustering: Analysis and an algorithm” (Links to an external site.)
-        by Andrew Y. Ng, Michael I. Jordan, Yair Weiss
-        This function computes k clusters in the graph contained in file with spectral clustering and returns a numpy
-        array of shape (number of vertices,) containing the label for each vertex, the Fiedler vector and
-        the adjacency matrix of the graph
-        :param G: nx graph
-        :param number_of_clusters_selection: either auto or manual, determines how k is selected
+ 
+        This function clusters a graph into either:
+        (1: get_optimal_k = True) the optimal number of clusters 
+        or
+        (2: get_optimal_k = False) k clusters.
+        It therefore computes the eigenvector of the laplacian and select corresponding eigenvectors which then 
+        will be clustered using K-means.
+        Th
+    
+        :param get_optimal_k: Determines whether a flexible or the optimal number of eigenvectors will be selected
         :param k: the number of clusters to be identified, works if selection method is manual
         :return: returns a numpy array of shape (number of vertices,) containing the label for each vertex,
-                        a numpy array of shape (number of vertices,) containing the Fiedler vector,
-                        a numpy matrix of shape (number of vertices, number of vertices) containing the adjacency matrix
+   
         """
 
 
@@ -62,38 +57,25 @@ class Spectral_clustering:
     
         # returns eigenvalues and vectors in ascending order
         values, vectors = linalg.eigh(L)
-        k_optimal  = np.argmax(np.abs(np.diff(values))) + 1
-        print(f"The optimal number of clusters is {(len(values) - k_optimal) -1}.") 
+        k_optimal  = np.argmax(np.abs(np.diff(values)))
+        k_optimal = len(values) - k_optimal -1
+        print(f"The optimal number of clusters is {k_optimal}.") 
 
         if get_optimal_k:
             k = k_optimal
-            X = vectors[:, k:]
-        else:
-            X = vectors[:, -k:]
+     
+        X = vectors[:, -k:]
+        # normalise the matrix formed by the eigvectors
         Y = X / np.linalg.norm(X, axis=1, keepdims=True)
-        result = KMeans(n_clusters=k).fit(Y).labels_
 
-       
+        clusters = KMeans(n_clusters=k).fit(Y).labels_
 
-        return result, self.A
+        return clusters
 
     def compute_Fiedler(self):
-        ''' Compute the Fiedler vector of the Laplacian matrix L = D - A'''
+        ''' Computes the Fiedler vector of the Laplacian matrix L = D - A'''
         A = nx.to_numpy_matrix(self.G)
         D = np.diagflat(np.sum(A, axis=1))
         laplace_mat = D - A
-        eigen_values, eigen_vecs = linalg.eigh(laplace_mat)
-        print(eigen_vecs)
+        eigen_values, eigen_vecs = linalg.eig(laplace_mat)
         return eigen_values, eigen_vecs[:, 1]
-
-if __name__ == '__main__':
-    import matplotlib.pyplot as plt 
-    file = 'example1.dat'
-    graph = read_graph(file)
-    clustering = Spectral_clustering(graph)
-    clustering.spectral_clustering(get_optimal_k = False, k = 10)
-    _, fiedler = clustering.compute_Fiedler()
-    plt.plot(np.sort(fiedler))
-    plt.xlabel("Node")
-    plt.ylabel("Eigenvector")
-    plt.show()
